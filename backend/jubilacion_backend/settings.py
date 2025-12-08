7f718aa38a654de3701a9525d04ffe5cfbe3e95c
Gerardo <gerardodemartini@gmail.com>
2025-12-08 13:48:16 -0300

    Fix: Remove syntax error in settings.py (extra parenthesis)

--- backend/jubilacion_backend/settings.py
+++ backend/jubilacion_backend/settings.py
@@ -146,7 +146,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
-    )
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
